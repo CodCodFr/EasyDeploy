230792ce1ad7056b95cfd198cbc5e0e028f19167
gaetanse <sadous.gaetan@gmail.com>
2025-08-12 15:10:20 +0200

    Refactor kill.sh to use consistent variable naming and improve service scaling logic

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Default value for the service name
-NAME="default_name"
+SERVICE_NAME="default_name"
 
 # Parse the arguments
 while getopts "f:" opt; do
   case $opt in
     f)
-      NAME=$OPTARG
+      SERVICE_NAME=$OPTARG
       ;;
     *)
       echo "Usage: $0 -f <service_name>"
@@ -16,6 +16,15 @@ while getopts "f:" opt; do
   esac
 done
 
-# Scale down and remove the service
-docker service scale "$NAME"=0
+# Get the number of replicas for the service
+REPLICAS=$(docker service inspect --format '{{.Spec.Mode.Replicated.Replicas}}' "$SERVICE_NAME")
+
+# Check if the replica count is greater than 0
+if [[ "$REPLICAS" -gt 0 ]]; then
+    echo "Scaling down '$SERVICE_NAME' from $REPLICAS to 0 replicas."
+    docker service scale "$SERVICE_NAME"=0
+else
+    echo "Service '$SERVICE_NAME' already has 0 replicas. Skipping."
+fi
+
 docker service rm "$NAME"
